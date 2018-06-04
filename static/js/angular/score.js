b85991f397c9a487a0e95445004d48e56775018e
Justin Duch <aphelion030@gmail.com>
2018-06-04 21:02:11 +1000

    loading screen

--- static/js/angular/score.js
+++ static/js/angular/score.js
@@ -7,12 +7,33 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
 
 app.controller("scoreCtrl", function($scope, $location, $http) {
     $scope.url = $location.absUrl().split('?')[1]
+
     $http({
         method : "GET",
         url : "analyse?" + $scope.url
     }).then(function(response) {
         $scope.callback = response.data;
-    }, function(response) {
-        $scope.callback = response.data;
     });
+
+    $scope.changeOrder = function(x) {
+        $scope.orderBy = x;
+    }
 });
+
+app.directive('loading', ['$http', function($http) {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            scope.loading = function () {
+                return $http.pendingRequests.length > 0;
+            };
+            scope.$watch(scope.loading, function(v) {
+                if(v) {
+                    elm.show();
+                } else {
+                    elm.hide();
+                }
+            });
+        }
+    };
+}]);
